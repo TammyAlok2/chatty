c8457bb9cd431a2cccfb911395a122ed79e843f0
Alok Tamrakar <109581769+TammyAlok2@users.noreply.github.com>
2024-11-04 16:21:07 +0530

    Started learning socket io from hitesh sir build simple chat app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,55 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {useState, useEffect} from 'react'
+import io from 'socket.io-client'
+import {nanoid} from 'nanoid'
+
+
+const socket = io.connect('http://localhost:3000')
 
 function App() {
+  const [messages, setMessages] = useState('');
+  const [chat,setChat] = useState([]);
+  const username = nanoid(4)
+
+  const sendChat =(e)=>[
+    e.preventDefault(),
+    socket.emit('chatWithAnkita',{id:username,message:messages}),
+    setMessages('')
+  ]
+
+
+  useEffect(()=>{
+socket.on("chatWithAnkita",(payload)=>{
+  setChat([...chat,payload])
+})
+  })
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Chat With Ankita</h1>
+
+        {
+          chat.map((payload,index)=>{
+            return(
+              <div key={index}>
+               
+                <h3>{payload.message} <span> {payload.id}</span></h3>
+              </div>
+            
+          )
+        })
+        }
+
+        <form onSubmit={sendChat}>
+          <input type="text" placeholder="Type a message" onChange={(e)=>
+          {
+            setMessages(e.target.value)}
+
+          } value={messages} />
+          <button type='submit' >Send</button>
+        </form>
       </header>
     </div>
   );
